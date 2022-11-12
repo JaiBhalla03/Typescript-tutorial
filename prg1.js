c042b9234dd1c804a7b3dbefb2ae0caa6e588676
jai <jai.bhalla2021@vitstudent.ac.in>
2022-11-12 08:37:53 +0530

    first commit

---
+++ prg1.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var user = { name: 'Jai', age: 20 };
+console.log(user.name + " " + user.age);
+//variables
+var greetings = "Hello World!";
+console.log(greetings);
---
+++ prg1.ts
@@ -0,0 +1,8 @@
+let user = {name: 'Jai', age: 20}
+console.log(user.name+" "+user.age)
+
+//variables
+let greetings: string = "Hello World!";
+console.log(greetings);
+
+export {}
